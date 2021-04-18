d2c6451473710da06c46d11894bf6beb6b1bc03e
prashant <pkapde308@gmail.com>
2021-04-18 21:05:54 +0530

    Refactored:removed if else statement and used switch

--- EmpWage.js
+++ EmpWage.js
@@ -30,3 +30,25 @@ if(empCheck == IS_PART_TIME) {
 	console.log("Not present");
 	empHrs = 0;
 }
+
+//UC 3
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
+let EMP_RATE_PER_HOUR = 20;
+let empHrs = 0;
+let empWage = 0;
+
+let empCheck = Math.floor(Math.random()*3);
+switch(empCheck) {
+	case IS_PART_TIME:
+		empHrs = 4;
+		break;
+	case IS_FULL_TIME:
+		empHrs = 8;
+		break;
+	default:
+		empHrs = 0;
+}
+
+empWage = empHrs*EMP_RATE_PER_HOUR;
+console.log(empWage)
