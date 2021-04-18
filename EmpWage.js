d4e955462fff527d635c2509934016e04df04df4
prashant <pkapde308@gmail.com>
2021-04-18 21:09:30 +0530

    Added:code to calculate wage based on working hour in month

--- EmpWage.js
+++ EmpWage.js
@@ -78,3 +78,30 @@ empWage = empHrs*EMP_RATE_PER_HOUR;
 totalWage += empWage;
 }
 console.log(totalWage)
+
+//UC 5
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
+let MAX_HRS_IN_MONTH = 10;
+let EMP_RATE_PER_HOUR = 20;
+let NUM_OF_WORKING_DAYS = 2;
+
+let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWage = 0;
+let totalWorkingDays = 0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+	totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+	let empCheck = Math.floor(Math.random()*3);
+	totalWorkingDays++;totalEmpHrs
+	switch(empCheck) {
+		case IS_PART_TIME:
+			empHrs = 4;
+		default:
+			empHrs = 0;
+	}
+	totalEmpHrs += empHrs;
+}
+totalWage = totalEmpHrs*EMP_RATE_PER_HOUR;
+console.log(totalWage);
