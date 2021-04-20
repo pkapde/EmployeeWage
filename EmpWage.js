88a9648f7082409b5a08fd1d05182d4db939261e
prashant <pkapde308@gmail.com>
2021-04-20 15:53:43 +0530

    Added:code to use arrow functions

--- EmpWage.js
+++ EmpWage.js
@@ -260,3 +260,27 @@ function totalWages(totalWage, dailyWage)
    return totalWage + dailyWage;
 }
 console.log("UC7A - Emp Wage Map totalHrs: " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+//UC9 Arrow functions
+const findTotal = (totalVal,dailyVal)=>{
+    return totalVal+dailyVal;
+}
+let count =0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+let totalSalary = empDailyWageArray.filter(dailyWage=>dailyWage>0)
+                                    .reduce(findTotal,0);
+console.log("UC 9A : Wage with arrow => function: "+totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value,key)=>{
+    if(value==8) 
+        fullWorkingDays.push(key);
+    else if(value==4)
+        partWorkingDays.push(key);
+    else 
+        nonWorkingDays.push(key);
+});
+console.log("Full working days: "+fullWorkingDays);
+console.log("Part working days: "+partWorkingDays);
+console.log("Non working days: "+nonWorkingDays);
