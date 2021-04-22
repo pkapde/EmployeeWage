3de110f77d19205a7249717c50485e430628c9df
prashant <pkapde308@gmail.com>
2021-04-23 03:40:40 +0530

    Added:code to validate pincode and email using regex

--- EmpPayroll.js
+++ EmpPayroll.js
@@ -8,6 +8,8 @@ class EmployeePayrollData{
         this.salary = params[2];
         this.gender = params[3];
         this.startDate = params[4];
+        this.pinCode = params[5];
+
     }
 
     //getter and setter
@@ -58,25 +60,40 @@ class EmployeePayrollData{
             this._startDate = startDate;
         else throw "Date is incorrect: "+startDate.toLocaleDateString("en-IN");
     }
+    get pinCode(){
+        return this._pinCode;
+    }
+    set pinCode(){
+        let pinCode = RegExp('^[1-9]{1}[0-9]{2}\\s{0, 1}[0-9]{3}$');
+        if(nameRegex.test(pinCode)){
+            this._pinCode = pinCode;
+            }
+            else throw 'Pin Is Incorrect';
+    }
+    get email(){
+        return this._email;
+    }
+    set email(){
+        let email = RegExp('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}');
+        if(nameRegex.test(email)){
+            this._email = email;
+            }
+            else throw 'email Is Incorrect';
+    }
 
     //toString
     toString(){
         const options = {year: 'numeric', month: 'numeric', day:'numeric'};
         const empDate = this.startDate == undefined ? "undefined":
                         this.startDate.toLocaleDateString("en-IN",options);
-        return "id = "+this.id+",name = "+this.name+",salary = "+this.salary +",gender = "+this.gender+",start date = "+empDate;
+        return "id = "+this.id+",name = "+this.name+",salary = "+this.salary +",gender = "+this.gender+",start date = "+empDate+",pinCode = "+this._pinCode;
     }
 }
 
 let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
 //Fail case for name
-try{
-    employeePayrollData.name = "john";
-    console.log(employeePayrollData.toString());
-}catch(e){
-    console.error(e);
-}
+
 let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayrollData.toString());
 
@@ -101,6 +118,69 @@ try{
 try{
     let newEmployeePayrollData3 = new EmployeePayrollData(2,"Mark",2000,"M",new Date('2021-04-11T10:20:30Z'));
     console.log(newEmployeePayrollData3.toString());
+} catch(e){
+    console.log(e);
+}
+//test case for 6 digit pincode
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(2,"Mark",2500,"M",new Date('2021-04-11T10:20:30Z'),400021);
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case for taking alphabets at starting
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(3,"joy",2550,"M",new Date('2021-04-11T10:20:30Z'),"A00021");
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case for taking alphabets at ending
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(3,"joy",2550,"M",new Date('2021-04-11T10:20:30Z'),"400020B");
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case for taking 400 222 valid
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(4,"john",2555,"M",new Date('2021-04-11T10:20:30Z'),"400 020");
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case for taking valid email abc@xyz.com
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(5,"juli",2560,"M",new Date('2021-04-11T10:20:30Z'),400020,"abc@.");
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case to validate second part ie bridgelabz
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(5,"juli",2560,"M",new Date('2021-04-11T10:20:30Z'),400020,"abc@bridgelabz.");
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case to validate .co part
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(5,"juli",2560,"M",new Date('2021-04-11T10:20:30Z'),400020,"abc@bridgelabx.co");
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case to validate abc.xyz@bridgelabz
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(5,"juli",2560,"M",new Date('2021-04-11T10:20:30Z'),400020,"abc.xyz@bridgelabz.co");
+    console.log(newEmployeePayrollData.toString());
+} catch(e){
+    console.log(e);
+}
+//test case to validate country code ie IN
+try{
+    let newEmployeePayrollData = new EmployeePayrollData(5,"juli",2560,"M",new Date('2021-04-11T10:20:30Z'),400020,"abc.xyz@bridgelabz.co.in");
+    console.log(newEmployeePayrollData.toString());
 } catch(e){
     console.log(e);
 }
