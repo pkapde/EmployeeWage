874296d25ed7bddbb5744d17b8d32c5c2bac81a1
prashant <pkapde308@gmail.com>
2021-04-23 02:43:08 +0530

    Added:Ability to check id,gender,salary and date using Regex

--- EmpPayroll.js
+++ EmpPayroll.js
@@ -15,20 +15,28 @@ class EmployeePayrollData{
         return this._id;
     }
     set id(id){
-        console.log("setting id :"+id);
-        this._id = id;
+        let idRegex = RegExp('^[1-9][0-9]{0,}$');
+        if(idRegex.test(id))
+            this._id = id;
+        else throw 'Id is incorrect: '+id;
     }
     get salary(){
         return this._salary;
     }
     set salary(salary){
-        this._salary = salary;
+        let salaryRegex = RegExp("^[1-9][0-9]{0,}$");
+        if(salaryRegex.test(salary))
+            this._salary = salary;
+        else throw 'Salary is incorrect: '+salary;
     }
     get gender(){
         return this._gender;
     }
     set gender(gender){
-        this._gender = gender;
+        let genderRegex = RegExp("^[MF]$");
+        if(genderRegex.test(gender))
+            this._gender = gender;
+        else throw "Gender incorrect: "+gender+". Choose M or F";
     }
     get name(){
         this._name = _name;
@@ -40,10 +48,20 @@ class EmployeePayrollData{
         }
         else throw 'Name Is Incorrect';
     }
+    get startDate(){
+        return this._startDate;
+    }
+    set startDate(){
+        if(startDate.getMonth()<=(new Date()).getMonth()
+           &&startDate.getDay()<=(new Date()).getDay()
+           &&startDate.getFullYear()<=(new Date()).getFullYear())
+            this._startDate = startDate;
+        else throw "Date is incorrect: "+startDate.toLocaleDateString("en-IN");
+    }
 
     //toString
     toString(){
-        const options = {year: 'numeric', month: 'long', day:'numeric'};
+        const options = {year: 'numeric', month: 'numeric', day:'numeric'};
         const empDate = this.startDate == undefined ? "undefined":
                         this.startDate.toLocaleDateString("en-IN",options);
         return "id = "+this.id+",name = "+this.name+",salary = "+this.salary +",gender = "+this.gender+",start date = "+empDate;
@@ -52,6 +70,7 @@ class EmployeePayrollData{
 
 let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
+//Fail case for name
 try{
     employeePayrollData.name = "john";
     console.log(employeePayrollData.toString());
@@ -60,3 +79,28 @@ try{
 }
 let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayrollData.toString());
+
+let newEmployeePayrollData2 = new EmployeePayrollData(1,"Terr",30000,"F",new Date());
+console.log(newEmployeePayrollData2.toString());
+
+//Fail case for salary
+try{
+    let newEmployeePayrollData3 = new EmployeePayrollData(2,"Mark",-2000,"F",new Date());
+    console.log(newEmployeePayrollData3.toString());
+} catch(e){
+    console.error(e);
+}
+//Fail case for gender
+try{
+    let newEmployeePayrollData3 = new EmployeePayrollData(2,"Mark",2000,"G",new Date());
+    console.log(newEmployeePayrollData3.toString());
+} catch(e){
+    console.error(e);
+}
+//Fail Case for date
+try{
+    let newEmployeePayrollData3 = new EmployeePayrollData(2,"Mark",2000,"M",new Date('2021-04-11T10:20:30Z'));
+    console.log(newEmployeePayrollData3.toString());
+} catch(e){
+    console.log(e);
+}
